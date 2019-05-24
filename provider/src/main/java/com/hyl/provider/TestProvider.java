b4601561fef054bbbe0c9aabbefb98793a17140b
hyl1995 <592443616@qq.com>
2019-05-24 14:23:52 +0800

    hyl

--- provider/src/main/java/com/hyl/provider/TestProvider.java
+++ provider/src/main/java/com/hyl/provider/TestProvider.java
@@ -6,7 +6,7 @@ public class TestProvider {
     public static void main(String[] arg) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:provider.xml");
 
-        //挂起当前线程，如果没有改行代码，服务提供者进程会消亡，服务消费者就发现不了提供者了
+        //挂起当前线程，如果没有该行代码，服务提供者进程会消亡，服务消费者就发现不了提供者了
         Thread.currentThread().join();
     }
 }
